7856682850cf947d4e440fd20ab12b8737f348ad
Georgios Roros <georgiosroros@georgioss-mbp-2.lan>
2022-08-05 18:51:47 -0600

    update step function in serverless

--- src/echo_handler.py
+++ src/echo_handler.py
@@ -1,7 +1,8 @@
-import json
+import json, os
 
 def echo_handler(event, context):
 
+    print(os.environ.get("WORKFLOW_FILE"))
     return {
         'statusCode': 200,
         'body': json.dumps(event),
