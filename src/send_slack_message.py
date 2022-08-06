7f6c92f9ba244ab90962db690dd56e2bd5c49020
Georgios Roros <georgiosroros@georgioss-mbp-2.lan>
2022-08-05 18:10:42 -0600

    remove breakpoint from send slack

--- src/send_slack_message.py
+++ src/send_slack_message.py
@@ -7,7 +7,6 @@ def send_slack_message(event, context):
         "Content-Type": "application/json"
     }
     body = json.dumps(event) 
-    breakpoint()
     requests.request("POST", pipe_dream_url, headers=headers, data=body)
     return {"statusCode": 200}
 
